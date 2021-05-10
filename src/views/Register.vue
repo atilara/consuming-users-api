62d997e99bc000cd57c56c5c22f98958a59481de
Átila Rodrigues <atila.rodriguesal@gmail.com>
2021-05-10 14:39:20 -0300

    feat: add route to register users

---
+++ src/views/Register.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Registro de Usuário</h1>
+    <div class="columns is-centered">
+      <div class="column is-half">
+        <input type="text" placeholder="Nome de Usuário" class="input mt-4 mb-2">
+        <input type="email" placeholder="email@email.com" class="input mb-2">
+        <input type="password" placeholder="Senha" class="input mb-2">
+        <button class="button">Cadastrar</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+
+</style>
