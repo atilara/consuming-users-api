2b3dc87892fbff875077dc050ab697a1d128b529
Átila Rodrigues <atila.rodriguesal@gmail.com>
2021-05-12 21:53:50 -0300

    feat: add axios request to test the connection

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -3,18 +3,35 @@
     <h1>Registro de Usuário</h1>
     <div class="columns is-centered">
       <div class="column is-half">
-        <input type="text" placeholder="Nome de Usuário" class="input mt-4 mb-2">
-        <input type="email" placeholder="email@email.com" class="input mb-2">
-        <input type="password" placeholder="Senha" class="input mb-2">
-        <button class="button">Cadastrar</button>
+        <input type="text" placeholder="Nome de Usuário" class="input mt-4 mb-2" v-model="name">
+        <input type="email" placeholder="email@email.com" class="input mb-2" v-model="email">
+        <input type="password" placeholder="Senha" class="input mb-2" v-model="password">
+        <button class="button" @click="register">Cadastrar</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  
+  data() {
+    return {
+      name: '',
+      email: '',
+      password: '',
+    }
+  },
+  methods: {
+    register() {
+      axios.get("http://localhost:5500/").then((res) => {
+        console.log(res);
+      }).catch((error) => {
+        console.log(error);
+      })
+    }
+  }
 }
 </script>
 
