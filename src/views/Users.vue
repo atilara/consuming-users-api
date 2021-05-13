c395c4a18d205f62df7d52bd7b03e0444677eeb4
Átila Rodrigues <atila.rodriguesal@gmail.com>
2021-05-13 14:17:39 -0300

    feat: add list of users

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,12 +1,57 @@
 <template>
   <div>
-    <h1>Users</h1>
+    <h1>Lista de Usuários</h1>
+    <div class="columns is-centered mt-5">
+      <table class="table is-half">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>E-mail</th>
+            <th>Nome</th>
+            <th>Cargo</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody v-for="user in users" :key="user.id">
+          <tr>
+            <td>{{user.id}}</td>
+            <td>{{user.email}}</td>
+            <td>{{user.name}}</td>
+            <td v-if="user.role == 0">Comum</td>
+            <td v-else>Administrador</td>
+            <td>
+              <button class="button is-warning mr-2">Editar</button>
+              <button class="button is-danger">Apagar</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  
+  created() {
+    var req = {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      }
+    };
+    
+    axios.get("http://localhost:5500/user", req).then((res) => {
+      this.users = res.data;
+    }).catch((error) => {
+      console.log(error);
+    })
+  },
+  data() {
+    return {
+      users: []
+    }
+  }
 }
 </script>
 
