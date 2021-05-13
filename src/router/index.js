af37d8bac6a8fe49ddf14eefa8abd7083611c69e
Átila Rodrigues <atila.rodriguesal@gmail.com>
2021-05-12 22:29:39 -0300

    feat: add view to login

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Register from "../views/Register.vue";
+import Login from "../views/Login.vue";
 
 Vue.use(VueRouter);
 
@@ -24,6 +25,11 @@ const routes = [
     path: "/register",
     name: "Register",
     component: Register,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
   },
 ];
 
---
+++ src/views/Login.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <div class="columns is-centered">
+      <div class="column is-half">
+        <div v-if="error != undefined">
+          <p class="mt-4">{{error}}</p>
+        </div>
+        <input type="email" placeholder="email@email.com" class="input mt-4 mb-2" v-model="email">
+        <input type="password" placeholder="Senha" class="input mb-2" v-model="password">
+        <button class="button" @click="login">Login</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      error: undefined
+    }
+  },
+  methods: {
+    login() {
+      console.log("Login!");
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
